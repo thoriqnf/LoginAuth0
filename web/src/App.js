037f96fb0b5f891e0b956ec03ee8a8661c4b1f98
thoriqnf <thoriqnfaizal@gmail.com>
2017-12-11 13:58:52 +0700

    installing auth0 bootstrap and react bootstrap

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import AuthLock from "auth0-lock";
+import { Grid, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
